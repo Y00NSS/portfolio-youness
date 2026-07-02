63071caa1c5d6d5a1c44ce6e309cc302569b9c5b
Youness EL MOURID <elmouridyouness58@gmail.com>
2026-07-02 19:20:15 +0100

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Youness El Mourid</h1>
+      <p>Étudiant en Électronique, Énergie Électrique et Automatique (EEA)</p>
+    </main>
+  );
+}
