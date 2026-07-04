7ec2b28e728472733dbf0df74aec462b07574511
Youness EL MOURID <elmouridyouness58@gmail.com>
2026-07-04 11:09:40 +0100

    Fix custom domain config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portfolio-youness",
-  assetPrefix: "/portfolio-youness/",
 };
 
 export default nextConfig;
